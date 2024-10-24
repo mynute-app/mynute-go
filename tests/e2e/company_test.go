998075cf7491cb6ea44c5a2c2971daadeded515e
troyaks1 <lgotcfg@gmail.com>
2024-10-24 13:39:23 -0300

    Creating new approach for company testing and will expand it to the others too.

--- tests/e2e/company_test.go
+++ tests/e2e/company_test.go
@@ -12,16 +12,16 @@ type Company struct {
 	T *testing.T
 }
 
-type companyActios struct {
+type companyActions struct {
 	*Company
 	Testers []handlers.Tester
 }
 
-func (c *Company) Init() *companyActios {
-	return &companyActios{Company: c}
+func (c *Company) Init() *companyActions {
+	return &companyActions{Company: c}
 }
 
-func (c *companyActios) LoadTester(n int) *companyActios {
+func (c *companyActions) GenerateTesters(n int) *companyActions {
 	for i := 0; i < n; i++ {
 		company := handlers.Tester{
 			Entity:      "company",
@@ -35,6 +35,87 @@ func (c *companyActios) LoadTester(n int) *companyActios {
 		}
 		c.Testers = append(c.Testers, company)
 	}
+	return c
+}
+
+func (c *companyActions) CreateAllTesters(s int) *companyActions {
+	for _, tester := range c.Testers {
+		c.T.Run("CreateCompany", tester.ExpectStatus(201).POST)
+	}
+	return c
+}
+
+func (c *companyActions) GetAllTesters(s int) *companyActions {
+	for _, tester := range c.Testers {
+		c.T.Run("GetCompany", tester.ExpectStatus(200).GET)
+	}
+	return c
+}
+
+func (c *companyActions) ForceGetAllTesters(s int) *companyActions {
+	for _, tester := range c.Testers {
+		c.T.Run("ForceGetCompany", tester.ExpectStatus(200).ForceGET)
+	}
+	return c
+}
+
+func (c *companyActions) UpdateAllTesters(s int) *companyActions {
+	for _, tester := range c.Testers {
+		c.T.Run("UpdateCompany", tester.ExpectStatus(200).PATCH)
+	}
+	return c
+}
+
+func (c *companyActions) DeleteAllTesters(s int) *companyActions {
+	for _, tester := range c.Testers {
+		c.T.Run("DeleteCompany", tester.ExpectStatus(204).DELETE)
+	}
+	return c
+}
+
+func (c *companyActions) ForceDeleteAllTesters(s int) *companyActions {
+	for _, tester := range c.Testers {
+		c.T.Run("ForceDeleteCompany", tester.ExpectStatus(204).ForceDELETE)
+	}
+	return c
+}
+
+func (c *companyActions) RunAll() {
+	c.
+		GenerateTesters(10).
+		CreateAllTesters(201).
+		UpdateAllTesters(200).
+		GetAllTesters(200).
+		ForceGetAllTesters(200).
+		DeleteAllTesters(204).
+		GetAllTesters(404).
+		ForceGetAllTesters(200).
+		ForceDeleteAllTesters(204).
+		ForceGetAllTesters(404)
+}
+
+func (c *companyActions) GetTester(id int, s int) *companyActions {
+	c.T.Run("GetCompany", c.Testers[id].ExpectStatus(s).GET)
+	return c
+}
+
+func (c *companyActions) ForceGetTester(id int, s int) *companyActions {
+	c.T.Run("ForceGetCompany", c.Testers[id].ExpectStatus(s).ForceGET)
+	return c
+}
+
+func (c *companyActions) UpdateTester(id int, s int) *companyActions {
+	c.T.Run("UpdateCompany", c.Testers[id].ExpectStatus(s).PATCH)
+	return c
+}
+
+func (c *companyActions) DeleteTester(id int, s int) *companyActions {
+	c.T.Run("DeleteCompany", c.Testers[id].ExpectStatus(s).DELETE)
+	return c
+}
+
+func (c *companyActions) ForceDeleteTester(id int, s int) *companyActions {
+	c.T.Run("ForceDeleteCompany", c.Testers[id].ExpectStatus(s).ForceDELETE)
 	return c
 }
 
