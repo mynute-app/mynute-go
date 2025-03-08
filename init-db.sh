8a309ae08b9c4ab00fee95de4f34cf26dc114553
troyaks1 <lgotcfg@gmail.com>
2025-03-08 18:17:52 -0300

    Fixing issue that made the init-db.sh try to connect to root instead to the actual database.

--- init-db.sh
+++ init-db.sh
@@ -7,7 +7,9 @@ echo "Initializing PostgreSQL database script..."
 if [ "$APP_ENV" == "test" ]; then
   TEST_DB="${POSTGRES_DB}-${APP_ENV}"
   echo "Creating test database: $TEST_DB"
-  psql -U "$POSTGRES_USER" -c "CREATE DATABASE \"$TEST_DB\";"
+  psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE \"$TEST_DB\";"
+else
+  echo "APP_ENV is not 'test'. Skipping test database creation."
 fi
 
 echo "Database initialization script complete."
