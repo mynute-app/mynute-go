28a5850d26c0939fd2e3520cad6313937c8be369
troyaks1 <lgotcfg@gmail.com>
2025-03-10 23:01:43 -0300

    Adding more tests for company

--- core/tests/e2e/company_test.go
+++ core/tests/e2e/company_test.go
@@ -4,10 +4,11 @@ import (
 	"agenda-kaki-go/core"
 	"agenda-kaki-go/core/config/db/model"
 	handler "agenda-kaki-go/core/tests/handlers"
+	"fmt"
 	"testing"
 )
 
-type Company struct{
+type Company struct {
 	created model.Company
 }
 
@@ -20,6 +21,11 @@ func Test_Company(t *testing.T) {
 	user.Login(t)
 	company := &Company{}
 	company.Create(t, user)
+	company.Update(t, map[string]any{
+		"name": "Updated Company Name",
+	})
+	company.Get(t)
+	company.Delete(t)
 }
 
 func (c *Company) Create(t *testing.T, user *User) map[string]any {
@@ -36,5 +42,32 @@ func (c *Company) Create(t *testing.T, user *User) map[string]any {
 		Name:  http.ResBody["name"].(string),
 		TaxID: http.ResBody["tax_id"].(string),
 	}
+	c.created.ID = uint(http.ResBody["id"].(float64))
 	return http.ResBody
+}
+
+func (c *Company) Get(t *testing.T) map[string]any {
+	http := (&handler.HttpClient{}).SetTest(t)
+	http.Method("GET")
+	http.URL(fmt.Sprintf("/company/%d", c.created.ID))
+	http.ExpectStatus(200)
+	http.Send(nil)
+	return http.ResBody
+}
+
+func (c *Company) Update(t *testing.T, data map[string]any) map[string]any {
+	http := (&handler.HttpClient{}).SetTest(t)
+	http.Method("PATCH")
+	http.URL(fmt.Sprintf("/company/%d", c.created.ID))
+	http.ExpectStatus(200)
+	http.Send(data)
+	return http.ResBody
+}
+
+func (c *Company) Delete(t *testing.T) {
+	http := (&handler.HttpClient{}).SetTest(t)
+	http.Method("DELETE")
+	http.URL(fmt.Sprintf("/company/%d", c.created.ID))
+	http.ExpectStatus(200)
+	http.Send(nil)
 }
