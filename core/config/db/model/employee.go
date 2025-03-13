fb0457c038e31f7cd1dc06e74ce61745459a0dae
troyaks1 <lgotcfg@gmail.com>
2025-03-12 23:56:52 -0300

    Changing employee model to be more like an extension of user.

--- core/config/db/model/employee.go
+++ core/config/db/model/employee.go
@@ -1,16 +1,22 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Employee struct {
-	gorm.Model
-	Tags           []string      `gorm:"type:json" json:"tag"`
-	CompanyID      uint          `gorm:"not null;index;foreignKey:CompanyID;references:ID;constraint:OnDelete:CASCADE;" json:"company_id"`
-	Company        Company       `gorm:"constraint:OnDelete:CASCADE;"`
-	UserID         uint          `gorm:"not null;index;foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;" json:"user_id"`
-	User           User          `gorm:"constraint:OnDelete:CASCADE;"`
-	Branches       []Branch      `gorm:"many2many:employee_branches;"` // Many-to-many relation with Branch
-	Services       []Service     `gorm:"many2many:employee_services;"` // Many-to-many relation with Service
-	AvailableSlots []TimeRange   `gorm:"type:json" json:"available_slots"`
-	Appointments   []Appointment `gorm:"foreignKey:EmployeeID"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      gorm.DeletedAt `gorm:"index"`
+	CompanyID      uint           `gorm:"not null;index;foreignKey:CompanyID;references:ID;constraint:OnDelete:CASCADE;" json:"company_id"`
+	Company        Company        `gorm:"constraint:OnDelete:CASCADE;"`
+	UserID         uint           `gorm:"unique;not null;index;foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;" json:"user_id"`
+	User           User           `gorm:"constraint:OnDelete:CASCADE;"`
+	Branches       []Branch       `gorm:"many2many:employee_branches;"` // Many-to-many relation with Branch
+	Services       []Service      `gorm:"many2many:employee_services;"` // Many-to-many relation with Service
+	AvailableSlots []TimeRange    `gorm:"type:json" json:"available_slots"`
+	Appointments   []Appointment  `gorm:"foreignKey:EmployeeID"`
+	Tags           []string       `gorm:"type:json" json:"tag"`
 }
