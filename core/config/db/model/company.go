6bdd3f752c159c41e5a524ef5d0a720563820c34
troyaks <lgotcfg@gmail.com>
2025-04-30 16:48:20 -0300

    Going back on this idea.

--- core/config/db/model/company.go
+++ core/config/db/model/company.go
@@ -1,40 +1,14 @@
 package model
 
 import (
-	"agenda-kaki-go/core/lib"
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
-// Company represents a company entity
-type CompanyBase struct {
-	Name  string `gorm:"not null;unique" json:"name"`
-	TaxID string `gorm:"not null;unique" json:"tax_id"`
-}
-
-type CompanyMeta struct {
-	BaseModel
-	CompanyBase
-	SchemaName string `gorm:"type:varchar(100);not null;uniqueIndex" json:"schema_name"`
-}
-
-func (CompanyMeta) TableName() string { return "general.companies" }
-
 type Company struct {
-	CompanyBase
-	ID        uuid.UUID  `gorm:"type:uuid;primaryKey;<-:create" json:"id"`
-	Employees []Employee `gorm:"foreignKey:CompanyID;constraint:OnDelete:CASCADE" json:"employees"` // One-to-many relation with Client
-	Branches  []Branch   `gorm:"foreignKey:CompanyID;constraint:OnDelete:CASCADE" json:"branches"`  // One-to-many relation with Branch
-	Services  []Service  `gorm:"foreignKey:CompanyID;constraint:OnDelete:CASCADE" json:"services"`  // One-to-many relation with Service
-	SectorID  *uuid.UUID `json:"sector_id"`
-	Sector    *Sector    `gorm:"foreignKey:SectorID;constraint:OnDelete:SET NULL;"`
-}
-
-func (Company) TableName() string { return "company" }
-
-func (c *Company) BeforeUpdate(tx *gorm.DB) (err error) {
-	if tx.Statement.Changed("ID") {
-		return lib.Error.Company.IdUpdateForbidden
-	}
-	return nil
+	BaseModel
+	Name       string     `gorm:"not null;unique" json:"name"`
+	TaxID      string     `gorm:"not null;unique" json:"tax_id"`
+	SchemaName string     `gorm:"type:varchar(100);not null;uniqueIndex" json:"schema_name"`
+	SectorID   *uuid.UUID `json:"sector_id"`
+	Sector     *Sector    `gorm:"foreignKey:SectorID;constraint:OnDelete:SET NULL;"`
 }
