70a15e28543b7739bbc77e01695e0a3fd71592e5
troyaks <lgotcfg@gmail.com>
2025-05-05 12:17:47 -0300

    Creating subdomain table.

---
+++ core/config/db/model/subdomain.go
@@ -0,0 +1,14 @@
+package model
+
+import "github.com/google/uuid"
+
+type Subdomain struct {
+	BaseModel
+	Name      string    `gorm:"type:varchar(100);not null;uniqueIndex" json:"name"`
+	CompanyID uuid.UUID `gorm:"not null;index" json:"company_id"`
+	Company   *Company  `gorm:"foreignKey:CompanyID;constraint:OnDelete:CASCADE;" json:"company"`
+}
+
+func (Subdomain) TableName() string {
+	return "public.subdomains"
+}
