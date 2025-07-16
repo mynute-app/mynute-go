b8f2f59b840254bceb963a159b88969f782b6936
troyaks <lgotcfg@gmail.com>
2025-07-16 19:44:41 -0300

    Trying to fix employee not returning services filled.

--- core/config/api/dto/employee_work_schedule.go
+++ core/config/api/dto/employee_work_schedule.go
@@ -20,13 +20,13 @@ type CreateEmployeeWorkSchedule struct {
 //	@name			CreateEmployeeWorkRange
 //	@tag.name		dto.workrange.create_work_range
 type CreateEmployeeWorkRange struct {
-	EmployeeID uuid.UUID `json:"employee_id" example:"00000000-0000-0000-0000-000000000000"` // Employee ID
-	BranchID   uuid.UUID `json:"branch_id" example:"00000000-0000-0000-0000-000000000000"`   // Branch ID
-	Weekday    uint8     `json:"weekday" example:"1"`                                        // Weekday (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
-	StartTime  string    `json:"start_time" example:"09:00" format:"HH:mm"`                  // Start time (date ignored)
-	EndTime    string    `json:"end_time" example:"17:00" format:"HH:mm"`                    // End time (date ignored)
-	TimeZone   string    `json:"time_zone" example:"America/New_York"`                       // Timezone in IANA format, e.g., "America/New_York"
-	Services []ServiceID `json:"services" swaggertype:"array,object"` // List of services associated with the work range
+	EmployeeID uuid.UUID   `json:"employee_id" example:"00000000-0000-0000-0000-000000000000"` // Employee ID
+	BranchID   uuid.UUID   `json:"branch_id" example:"00000000-0000-0000-0000-000000000000"`   // Branch ID
+	Weekday    uint8       `json:"weekday" example:"1"`                                        // Weekday (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+	StartTime  string      `json:"start_time" example:"09:00" format:"HH:mm"`                  // Start time (date ignored)
+	EndTime    string      `json:"end_time" example:"17:00" format:"HH:mm"`                    // End time (date ignored)
+	TimeZone   string      `json:"time_zone" example:"America/New_York"`                       // Timezone in IANA format, e.g., "America/New_York"
+	Services   []ServiceID `json:"services" swaggertype:"array,object"`                        // List of services associated with the work range
 }
 
 type UpdateWorkRange struct {
@@ -40,6 +40,12 @@ type UpdateWorkRange struct {
 //	@name			EmployeeWorkRange
 //	@tag.name		dto.workrange.full
 type EmployeeWorkRange struct {
-	ID string `json:"id" example:"00000000-0000-0000-0000-000000000000"` // Work range ID
-	CreateEmployeeWorkRange
+	ID         string        `json:"id" example:"00000000-0000-0000-0000-000000000000"`          // Work range ID
+	EmployeeID uuid.UUID     `json:"employee_id" example:"00000000-0000-0000-0000-000000000000"` // Employee ID
+	BranchID   uuid.UUID     `json:"branch_id" example:"00000000-0000-0000-0000-000000000000"`   // Branch ID
+	Weekday    uint8         `json:"weekday" example:"1"`                                        // Weekday (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+	StartTime  string        `json:"start_time" example:"09:00" format:"HH:mm"`                  // Start time (date ignored)
+	EndTime    string        `json:"end_time" example:"17:00" format:"HH:mm"`                    // End time (date ignored)
+	TimeZone   string        `json:"time_zone" example:"America/New_York"`                       // Timezone in IANA format, e.g., "America/New_York"
+	Services   []ServiceBase `json:"services" swaggertype:"array,object"`                        // List of services associated with the work range
 }
