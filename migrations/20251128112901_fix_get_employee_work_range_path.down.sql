d67e2aef0e64c124771c15919abfead57162867a
troyaks <lgotcfg@gmail.com>
2025-12-04 19:59:08 -0300

    chore: remove old endpoint path migrations for clean initial setup
    - Delete endpoint path change migrations (November 28)
    - Keep only initial schema setup migration
    - Starting fresh with production database
    - Endpoint paths are already correct in initial schema

--- migrations/20251128112901_fix_get_employee_work_range_path.down.sql
+++
@@ -1,6 +0,0 @@
--- Rollback GetEmployeeWorkRange path fix
-UPDATE public.endpoints
-SET path = '/employee/:id/work_range/:work_range_id'
-WHERE controller_name = 'GetEmployeeWorkRangeById'
-  AND method = 'GET'
-  AND path = '/employee/:employee_id/work_range/:work_range_id';
--- migrations/20251128112901_fix_get_employee_work_range_path.up.sql
+++
@@ -1,6 +0,0 @@
--- Fix GetEmployeeWorkRange controller name mismatch
-UPDATE public.endpoints
-SET path = '/employee/:employee_id/work_range/:work_range_id'
-WHERE controller_name = 'GetEmployeeWorkRangeById'
-  AND method = 'GET'
-  AND path = '/employee/:id/work_range/:work_range_id';
