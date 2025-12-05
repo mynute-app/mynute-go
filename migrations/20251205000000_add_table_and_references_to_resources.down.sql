234ed4b1010c84b18ddc52d463f9749f9aebc62b
troyaks <lgotcfg@gmail.com>
2025-12-04 21:34:38 -0300

    refactor: remove initial schema setup and add missing columns to resources table

--- migrations/20251205000000_add_table_and_references_to_resources.down.sql
+++
@@ -1,7 +0,0 @@
--- Migration: add_table_and_references_to_resources (DOWN)
--- Created at: 20251205000000
--- Description: Remove table and references columns from resources table
-
-ALTER TABLE public.resources 
-DROP COLUMN IF EXISTS "table",
-DROP COLUMN IF EXISTS "references";
--- migrations/20251205000000_add_table_and_references_to_resources.up.sql
+++
@@ -1,7 +0,0 @@
--- Migration: add_table_and_references_to_resources
--- Created at: 20251205000000
--- Description: Add missing table and references columns to resources table
-
-ALTER TABLE public.resources 
-ADD COLUMN IF NOT EXISTS "table" VARCHAR(255),
-ADD COLUMN IF NOT EXISTS "references" JSONB;
